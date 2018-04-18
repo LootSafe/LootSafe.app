dd28376abb28c4c1c1e37496eb282caffb62ee29
Jeff M <jekrimo@Jeffs-MacBook-Pro.local>
2018-04-18 13:32:04 -0600

    missed a distribute

--- app/components/Admin/SpawnItem.js
+++ app/components/Admin/SpawnItem.js
@@ -150,7 +150,7 @@ export default class SpawnItem extends Component {
                   this.execute();
                 }}
               >
-                Spawn
+                Distribute
               </button>
             </div>
           </div>
